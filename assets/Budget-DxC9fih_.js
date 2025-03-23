5c5e45f3bfb02cb75b850d6b98c549ead168b982
Jeyreet <jeyreet@yandex.ru>
2025-03-23 20:46:37 +0700

    Updates

---
+++ assets/Budget-DxC9fih_.js
@@ -0,0 +1,1 @@
+import{u as t,r as s,j as r}from"./index-D_RwnVi_.js";const o=()=>{const{setHeaderTitle:e}=t();return s.useEffect(()=>{e("Бюджет")},[]),r.jsx("p",{children:"BUDGET"})};export{o as default};
---
+++ assets/Home-XzX8HGFC.js
@@ -0,0 +1,1 @@
+import{u as s,r as t,j as r}from"./index-D_RwnVi_.js";const a=()=>{const{setHeaderTitle:e}=s();return t.useEffect(()=>{e("Главная")},[]),r.jsx("p",{children:"HOME"})};export{a as default};
