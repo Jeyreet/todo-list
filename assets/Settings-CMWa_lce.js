2fdf81a0a7f4b1aa88730a26037489cc5c614cab
Jeyreet <jeyreet@yandex.ru>
2025-04-05 23:32:34 +0700

    Updates

--- assets/Settings-CMWa_lce.js
+++
@@ -1,1 +0,0 @@
-import{u as e,r as s,j as r}from"./index-D_RwnVi_.js";const o=()=>{const{setHeaderTitle:t}=e();return s.useEffect(()=>{t("Настройки")},[]),r.jsx("p",{children:"SETTINGS"})};export{o as default};
