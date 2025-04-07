75f3022e0ad605567e4579f84ef45bd802edb29f
Jeyreet <jeyreet@yandex.ru>
2025-04-08 04:59:48 +0700

    Updates

---
+++ assets/Home-DNgBSuMH.js
@@ -0,0 +1,1 @@
+import{u as s,r,j as o}from"./index-d113ghzT.js";const l=()=>{const e=s(t=>t.setHeaderTitle);return r.useEffect(()=>{e("Главная")},[]),o.jsx("p",{children:"HOME"})};export{l as default};
---
+++ assets/Title-CyPPYOBi.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-d113ghzT.js";const e="_Title_1r5ho_1",o={Title:e},l=({children:s})=>t.jsx("h3",{className:o.Title,children:s});export{l as T};
