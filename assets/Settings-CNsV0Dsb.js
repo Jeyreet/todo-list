e0381c2b934f1c269c50b84e22ccd87bdecb6899
Jeyreet <jeyreet@yandex.ru>
2025-04-07 21:06:50 +0700

    Updates

---
+++ assets/Settings-CNsV0Dsb.js
@@ -0,0 +1,1 @@
+import{u as s,r,j as a}from"./index-B3hIxIdI.js";const i=()=>{const e=s(t=>t.setHeaderTitle);return r.useEffect(()=>{e("Настройки")},[]),a.jsx("p",{children:"SETTINGS"})};export{i as default};
