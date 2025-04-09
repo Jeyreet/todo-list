bd79dad6313ea912ac204988c76df3da76b02854
Jeyreet <jeyreet@yandex.ru>
2025-04-10 00:36:44 +0700

    Updates

--- assets/Home-BQfDFBc0.js
+++
@@ -1,1 +0,0 @@
-import{u as s,r,j as o}from"./index-BCEI--hm.js";const l=()=>{const e=s(t=>t.setHeaderTitle);return r.useEffect(()=>{e("Главная")},[]),o.jsx("p",{children:"HOME"})};export{l as default};
