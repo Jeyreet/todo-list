2cc3c2c489ec7d1075f1af9c2722ce6779dfc2f2
Jeyreet <jeyreet@yandex.ru>
2025-04-13 09:37:46 +0700

    Updates

---
+++ assets/Home-BcE85mod.js
@@ -0,0 +1,1 @@
+import{u as s,r,j as o}from"./index-BKDHCSi8.js";const l=()=>{const e=s(t=>t.setHeaderTitle);return r.useEffect(()=>{e("Главная")},[]),o.jsx("p",{children:"HOME"})};export{l as default};
