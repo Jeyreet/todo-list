eaa104ba901e8d9e15afeb1b4c07b8c521aaa567
Jeyreet <jeyreet@yandex.ru>
2025-04-13 08:23:10 +0700

    Updates

---
+++ assets/Budget-Duq68VP3.js
@@ -0,0 +1,1 @@
+import{u as s,r,j as a}from"./index-DQvqQxX3.js";const u=()=>{const e=s(t=>t.setHeaderTitle);return r.useEffect(()=>{e("Бюджет")},[]),a.jsx("p",{children:"BUDGET"})};export{u as default};
--- assets/Budget-vVMEB_o1.js
+++
@@ -1,1 +0,0 @@
-import{u as s,r,j as a}from"./index-DAWsXvp0.js";const u=()=>{const e=s(t=>t.setHeaderTitle);return r.useEffect(()=>{e("Бюджет")},[]),a.jsx("p",{children:"BUDGET"})};export{u as default};
