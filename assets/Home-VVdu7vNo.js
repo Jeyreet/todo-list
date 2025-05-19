60788e25fbd995b8662037b0d576a84021a1b426
Jeyreet <jeyreet@yandex.ru>
2025-05-20 02:25:11 +0700

    Updates

--- assets/Home-VVdu7vNo.js
+++
@@ -1,1 +0,0 @@
-import{u as s,r,j as o}from"./index-8MgR8TBQ.js";const l=()=>{const e=s(t=>t.setHeaderTitle);return r.useEffect(()=>{e("Главная")},[]),o.jsx("p",{children:"HOME"})};export{l as default};
